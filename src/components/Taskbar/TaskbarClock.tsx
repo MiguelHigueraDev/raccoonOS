f58c2532e04e4c46949dd1b0bf11fd22552b0088
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-06-28 14:56:03 -0400

    feat: add dynamic date and time (synced with real time)

--- src/components/Taskbar/TaskbarClock.tsx
+++ src/components/Taskbar/TaskbarClock.tsx
@@ -1,11 +1,35 @@
-import classes from './TaskbarClock.module.css'
+import { useEffect, useState } from 'react';
+import classes from './TaskbarClock.module.css';
 const TaskbarClock = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const dateInterval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(dateInterval);
+  }, []);
+
+  const formatTime = (date: Date) => {
+    const minutes = String(date.getHours()).padStart(2, '0');
+    const seconds = String(date.getMinutes()).padStart(2, '0');
+    return `${minutes}:${seconds}`;
+  };
+
+  const formatDate = (date: Date) => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
   return (
     <div className={classes.clock}>
-        <time>14:45</time>
-        <time>28/06/2024</time>
+      <time>{formatTime(currentDate)}</time>
+      <time>{formatDate(currentDate)}</time>
     </div>
-  )
-}
+  );
+};
 
-export default TaskbarClock
+export default TaskbarClock;
