0c588350d2cfcb6dfae7ae90a038f2dedc87448d
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-11 22:36:24 -0400

    feat: add typing indicator

--- src/stores/ChatStore.ts
+++ src/stores/ChatStore.ts
@@ -3,6 +3,7 @@ import { create } from 'zustand';
 type Store = {
   messages: Message[];
   addMessage: (message: Message) => void;
+  removeIsTypingMessage: () => void;
 };
 
 export interface Message {
@@ -16,11 +17,19 @@ export default create<Store>((set) => ({
     {
       timestamp: Date.now(),
       sender: 'ai',
-      content: 'Hello! I\'m an AI clone of Miguel. Feel free to ask any questions, I\'ll try my best to answer them!',
+      content:
+        "Hello! I'm an AI clone of Miguel. Feel free to ask any questions, I'll try my best to answer them!",
     },
   ],
   addMessage: (message) =>
     set((state) => ({
       messages: [...state.messages, { ...message, timestamp: Date.now() }],
+    })),
+
+  removeIsTypingMessage: () =>
+    set((state) => ({
+      messages: state.messages.filter(
+        (message) => message.sender !== 'ai' || message.content !== 'Typing...'
+      ),
     })),
 }));
