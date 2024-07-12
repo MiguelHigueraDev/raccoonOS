0b3f6cafbed4d663cd577658cf5af984670ea476
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-11 22:22:01 -0400

    feat: improve mesage appearance

--- src/components/Applications/ChatApplication/ChatMessage.tsx
+++ src/components/Applications/ChatApplication/ChatMessage.tsx
@@ -4,15 +4,31 @@ import classes from './ChatApplication.module.css';
 const ChatMessage = ({ message }: { message: Message }) => {
   if (message.sender === 'user') {
     return (
-      <p>
-        <span className={classes.selfMessage}>You:</span> {message.content}
-      </p>
+      <div className={`${classes.selfMessage} ${classes.message}`}>
+        <div className="flex items-center gap-2 mb-1">
+          <img
+            src="./user.svg"
+            alt=""
+            className="rounded-full object-cover"
+          />
+          <span>You</span>
+        </div>
+        <p>{message.content}</p>
+      </div>
     );
   } else {
     return (
-      <p>
-        <span className={classes.aiMessage}>Miguel:</span> {message.content}
-      </p>
+      <div className={`${classes.aiMessage} ${classes.message}`}>
+        <div className="flex items-center gap-2 mb-1">
+          <img
+            src="./raccoonos-logo.webp"
+            alt=""
+            className="rounded-full object-cover"
+          />
+          <span>Miguel</span>
+        </div>
+        <p>{message.content}</p>
+      </div>
     );
   }
 };
