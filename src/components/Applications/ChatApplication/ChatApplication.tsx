4a55463d944238f11c8bc071a8d20b676f78802e
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-08-05 09:55:07 -0400

    fix(ChatApplication): change some dimensions

--- src/components/Applications/ChatApplication/ChatApplication.tsx
+++ src/components/Applications/ChatApplication/ChatApplication.tsx
@@ -106,7 +106,7 @@ const ChatApplication = ({ winProps }: { winProps: WindowProps }) => {
         handleClose={winProps.handleClose}
         handleHide={winProps.handleHide}
         width={1000}
-        height={860}
+        height={830}
         appName={winProps.appName}
         zIndex={winProps.zIndex}
         nonResizable
