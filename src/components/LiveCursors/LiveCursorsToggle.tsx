04abe8df662ae00b5f1be4b7f57760f260425a50
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2025-06-30 17:12:59 -0400

    fix: update LiveCursorsToggle to be more cohesive

--- src/components/LiveCursors/LiveCursorsToggle.tsx
+++ src/components/LiveCursors/LiveCursorsToggle.tsx
@@ -1,3 +1,5 @@
+import styles from "./LiveCursorsToggle.module.css";
+
 interface LiveCursorsToggleProps {
   showLiveCursors: boolean;
   setShowLiveCursors: React.Dispatch<React.SetStateAction<boolean>>;
@@ -9,23 +11,23 @@ const LiveCursorsToggle: React.FC<LiveCursorsToggleProps> = ({
 }) => {
   return (
     <div
-      className="live-cursors-toggle"
+      className={styles.liveCursorsToggle}
       onClick={() => setShowLiveCursors((prev) => !prev)}
-      style={{
-        position: "absolute",
-        fontSize: "12px",
-        bottom: "70px",
-        left: "20px",
-        backgroundColor: "black",
-        color: "white",
-        border: "1px solid white",
-        padding: "10px",
-        borderRadius: "5px",
-        cursor: "pointer",
-        zIndex: 1000,
-      }}
     >
-      {showLiveCursors ? "Hide Live Cursors" : "Show Live Cursors"}
+      <svg 
+        className={styles.icon} 
+        viewBox="0 0 24 24" 
+        fill="currentColor"
+      >
+        {showLiveCursors ? (
+          <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+        ) : (
+          <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
+        )}
+      </svg>
+      <span className={styles.text}>
+        {showLiveCursors ? "Hide" : "Show"} Live Cursors
+      </span>
     </div>
   );
 };
